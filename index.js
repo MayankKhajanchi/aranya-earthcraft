b9e2311e48f3a4a9f3d97ffbc166f313fe3c3154
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-03-18 16:16:34 +0530

    scroll setup

---
+++ index.js
@@ -0,0 +1,17 @@
+// new fullpage('#fullpage', {
+//   autoScrolling:true,
+//   // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+//   scrollHorizontally: true,
+// });
+console.log("hey");
+ //   $(window).scroll(function() {
+ //   	console.log("hey");
+ //    if ($(this).scrollTop()>0)
+ //     {
+ //      $('.navbar').fadeOut();
+ //     }
+ //    else
+ //     {
+ //      $('.navbar').fadeIn();
+ //     }
+ // });
