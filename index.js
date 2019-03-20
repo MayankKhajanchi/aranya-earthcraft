4c251f84caf9792116e291b0e927145570cc4310
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-03-20 12:34:41 +0530

    left and right arrows added

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("nav").style.top = "0";
-//   } else {
-//     document.getElementById("nav").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
 setTimeout(function () {
-  // $('#overlay').fadeOut('fast');
-  $('.down-arrow').hide();
+  $('.down-arrow, .left-arrow, .right-arrow').hide();
 }, 10000);
