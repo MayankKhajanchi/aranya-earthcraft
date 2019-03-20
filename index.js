024e7573b8f4a956a4b447343bd28f9534a0190d
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-03-20 12:22:53 +0530

    down arrow will disappear after 10 seconds

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-// new fullpage('#fullpage', {
-//   autoScrolling:true,
-//   // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
-//   scrollHorizontally: true,
-// });
-// console.log("hey");
-//   $(window).scroll(function() {
-//   	console.log("hey");
-//    if ($(this).scrollTop()>0)
-//     {
-//      $('.navbar').fadeOut();
-//     }
-//    else
-//     {
-//      $('.navbar').fadeIn();
-//     }
-// });
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("nav").style.top = "0";
+//   } else {
+//     document.getElementById("nav").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+setTimeout(function () {
+  // $('#overlay').fadeOut('fast');
+  $('.down-arrow').hide();
+}, 10000);
