c6983704e89692bcbe05c877fb89a9da30865e60
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-05-21 18:52:13 +0530

    fixes to mobile view

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ setTimeout(function () {
   $('.down-arrow, .left-arrow, .right-arrow').hide();
 }, 10000);
 
-function scroll() {
-  // window.scrollBy(0, 500);
-  console.log("hey");
-}
-
 function width(){
   if($(document).width() < 576){
   	console.log($(document).width())
@@ -15,7 +10,6 @@ function width(){
   	$(cat_carousel).addClass("order-12");
   	$(cat_side).addClass("order-1");
   }else if($(document).width() > 576){
-  	console.log($(document).width())
   	var cat_carousel = document.querySelector(".carousel-section");
   	var cat_side = document.querySelector(".side-section");
   	$(cat_carousel).addClass("order-1");
