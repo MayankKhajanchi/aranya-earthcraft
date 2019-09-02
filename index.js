a20df2df27928aeca874729a6e7a52f372a4dd8f
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-09-02 14:40:58 +0530

    js cleanup, modal for mobile screens

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function width() {
+$(document).ready(function () {
+  $('.bypass').click(function (event) {
+    const pic = $(this).closest('.section').find('.carousel-item.active img').attr('src');
+    $('.modal-body').css("background-image", `url(${pic})`);
+    $('#exampleModal').modal('toggle');
+  });
   if ($(document).width() <= 576) {
     var cat_carousel = document.querySelector(".carousel-section");
     var cat_side = document.querySelector(".side-section");
@@ -10,20 +15,8 @@ function width() {
     $(cat_carousel).addClass("order-1");
     $(cat_side).addClass("order-12");
   }
-  var mk = window.innerHeight;
-  $('.section').css('max-height', mk)
-}
-
-width();
-
-$(document).ready(function () {
-  $('.bypass').click(function (event) {
-    const pic = $(this).closest('.section').find('.carousel-item.active img').attr('src');
-    // $('.modal-body').html('<img src="' + pic + '" />')
-    console.log(pic);
-    $('.modal-body').css("background-image", `url(${pic})`);
-    $('#exampleModal').modal('toggle');
-  });
+  var ih = window.innerHeight;
+  $('.section').css('max-height', ih)
 });
 
 function rightScroll() {
