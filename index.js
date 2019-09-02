07c7df19e3bfd963fbf28288f2288ac373162014
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-09-02 13:10:47 +0530

    modal working

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function width(){
-  if($(document).width() <= 576){
-  	var cat_carousel = document.querySelector(".carousel-section");
-  	var cat_side = document.querySelector(".side-section");
-  	$(cat_carousel).addClass("order-12");
-  	$(cat_side).addClass("order-1");
-  }else if($(document).width() > 576){
-  	var cat_carousel = document.querySelector(".carousel-section");
-  	var cat_side = document.querySelector(".side-section");
-  	$(cat_carousel).addClass("order-1");
-  	$(cat_side).addClass("order-12");
+function width() {
+  if ($(document).width() <= 576) {
+    var cat_carousel = document.querySelector(".carousel-section");
+    var cat_side = document.querySelector(".side-section");
+    $(cat_carousel).addClass("order-12");
+    $(cat_side).addClass("order-1");
+  } else if ($(document).width() > 576) {
+    var cat_carousel = document.querySelector(".carousel-section");
+    var cat_side = document.querySelector(".side-section");
+    $(cat_carousel).addClass("order-1");
+    $(cat_side).addClass("order-12");
   }
   var mk = window.innerHeight;
   $('.section').css('max-height', mk)
@@ -16,24 +16,19 @@ function width(){
 
 width();
 
-$(document).ready(function() {
- $('.bypass').click(function(event) {
-  const pic = $(this).closest('.section').find('.carousel-item.active img').attr('src');
-  $('.modal-body').html('<img src="'+pic+'" />')
-  $('#exampleModal').modal('toggle');
-
-  // $('#book1 .picture').html('<img src="'+book1.pictureURL+'" />');
- });
-
+$(document).ready(function () {
+  $('.bypass').click(function (event) {
+    const pic = $(this).closest('.section').find('.carousel-item.active img').attr('src');
+    // $('.modal-body').html('<img src="' + pic + '" />')
+    console.log(pic);
+    $('.modal-body').css("background-image", `url(${pic})`);
+    $('#exampleModal').modal('toggle');
+  });
 });
 
-$( document ).ready(function() {
-  $(".product-name").offset().top
-});
-
-function rightScroll(){
-  document.getElementById('master-container').scrollBy(10,0)
+function rightScroll() {
+  document.getElementById('master-container').scrollBy(10, 0)
 }
-function leftScroll(){
-  document.getElementById('master-container').scrollBy(-10,0)
+function leftScroll() {
+  document.getElementById('master-container').scrollBy(-10, 0)
 }
